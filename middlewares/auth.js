76e57024259b353296d6d8d93c97a2852f1ee81a
jessesongok <jessesongok@gmail.com>
2021-02-08 10:31:16 +0300

    Fixed JWT middleware bug

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,18 +4,13 @@ const AuthMiddleware = (req,res,next)=>{
     const AuthHeader = req.headers.authorization
             const token = AuthHeader.split(' ')[1]
 
-
-   try {
-        const token = AuthHeader.split(' ')[1]
-        const decoded = jwt.verify(token, process.env.JWT_KEY)
-        req.userId = decoded.email
-        console.log(decode.email)
-        next()
-    } catch(e){
-        res.status(401).send({
-            "Error": "Invalid Token"       
-        })
-    }
+if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
+  
+  jwt.verify(token, process.env.JWT_KEY, (err, decoded)=>{
+    if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+    req.userId = decoded.id;
+    res.status(200)
+  });
 
 }
 module.exports = AuthMiddleware
